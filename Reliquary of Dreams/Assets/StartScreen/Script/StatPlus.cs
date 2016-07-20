0134896712da6d8ab0adc99d701c74d00e1a51f2
dbga_06 <dbga_06@dbga.local>
2016-07-20 18:26:27 +0200

    roba a caso di niciolas

--- Reliquary of Dreams/Assets/StartScreen/Script/StatPlus.cs
+++ Reliquary of Dreams/Assets/StartScreen/Script/StatPlus.cs
@@ -3,23 +3,39 @@ using System.Collections;
 using UnityEngine.UI;
 public class StatPlus : MonoBehaviour
 {
+    private int point = 24;
+    public Text pointText;
     public Text numberText;
     private int number = 1;
-    public int numberMax;
     public void ClickPlus()
     {
         if (number < 10)
-        {
+        {   
             number++;
-            numberText.text = "" + number;
+            numberText.text = "" + number;                       
+        {
+        if (point < 25 && point > 0)
+        {
+            point--;                   
+            pointText.text = "" + point;
         }
     }
-    public void ClickMinus()
+  }
+}
+    
+       public void ClickMinus()
     {
         if (number > 1)
         {
             number--;
             numberText.text = "" + number;
+
+          if (point > 0 && point < 24)
+            {
+                point++;
+                pointText.text = "" + point;
+            }
         }
     }
 } 
+
