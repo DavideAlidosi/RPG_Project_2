bf9b3d2a899b90ad5ff20e32827b10098411927f
dbga_06 <dbga_06@dbga.local>
2016-07-21 10:24:22 +0200

    tooltip

--- Reliquary of Dreams/Assets/Script/UI/ShowTool.cs
+++ Reliquary of Dreams/Assets/Script/UI/ShowTool.cs
@@ -7,9 +7,14 @@ public class ShowTool : MonoBehaviour {
     public Text strTxt;
     public Text cosTxt;
     public Text hpTxt;
+    public Text intTxt;
+    public Text perTxt;
+    public Text agiTxt;
+    public Text forTxt;
 
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
 	    
 	}
 	
