bac6f97109c1a8313e154e3d1db26e242b9d3be1
dbga_28 <dbga_28@dbga.local>
2016-07-25 15:49:22 +0200

    Add Avaiable animation Implement Idle > Walk in animator under AnimatioTester sceme

---
+++ Reliquary of Dreams/Assets/Script/AnimationTester.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationTester : MonoBehaviour {
+
+    public Animator animator;
+    public int speed;
+
+    bool facingRight = true;
+
+	void Update () {
+
+        int currentSpeed = 0;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            currentSpeed = speed;
+            facingRight = true;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            currentSpeed = -speed;
+            facingRight = false;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        transform.Translate(transform.right * currentSpeed * Time.deltaTime); //Definisco il movimento per il tempo
+
+        animator.SetInteger("XSpeed", currentSpeed);
+        animator.SetBool("FacingRight", facingRight);
+
+    }
+}
