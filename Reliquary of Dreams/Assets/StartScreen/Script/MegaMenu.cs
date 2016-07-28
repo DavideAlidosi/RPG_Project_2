142ed12d578c161044389abf1d04b2d8ca4281cb
dbga_06 <dbga_06@dbga.local>
2016-07-28 14:42:01 +0200

    Nuovo menu su un unica scena

---
+++ Reliquary of Dreams/Assets/StartScreen/Script/MegaMenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MegaMenu : MonoBehaviour
+{
+    public GameObject canvasCrediti;
+    public GameObject canvasMenu;
+    public GameObject canvasOpzioni;
+    public GameObject canvasClassi;
+
+    void Awake()
+    {
+        canvasCrediti.SetActive(false);
+        canvasMenu.SetActive(true);
+        canvasOpzioni.SetActive(false);
+        canvasClassi.SetActive(false);
+    } 
+
+    public void Crediti ()
+    {
+        canvasCrediti.SetActive(true);
+        canvasMenu.SetActive(false);
+        canvasOpzioni.SetActive(false);
+        canvasClassi.SetActive(false);
+    }
+    public void Menu()
+    {
+        canvasCrediti.SetActive(false);
+        canvasMenu.SetActive(true);
+        canvasOpzioni.SetActive(false);
+        canvasClassi.SetActive(false);
+    }
+    public void Opzioni()
+    {
+        canvasCrediti.SetActive(false);
+        canvasMenu.SetActive(false);
+        canvasOpzioni.SetActive(true);
+        canvasClassi.SetActive(false);
+    }
+    public void Classi()
+    {
+        canvasCrediti.SetActive(false);
+        canvasMenu.SetActive(false);
+        canvasOpzioni.SetActive(false);
+        canvasClassi.SetActive(true);
+    }
+}
